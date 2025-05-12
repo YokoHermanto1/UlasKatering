95ce891037201f7c8b756a341ae5e47e295cf017
YokoHermanto1 <yokohermanto@gmail.com>
2025-05-12 17:36:40 +0700

    Menambahkan Gambar

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -2,14 +2,8 @@
   <!-- Container utama -->
   <div class="q-px-lg q-mt-xl flex flex-center column">
     <!-- Search Bar -->
-    <div class="full-width" style="max-width: 550px;">
-      <q-input
-        v-model="searchQuery"
-        placeholder="Cari katering"
-        outlined
-        rounded
-
-      >
+    <div class="full-width" style="max-width: 550px">
+      <q-input v-model="searchQuery" placeholder="Cari katering" outlined rounded>
         <template v-slot:append>
           <q-icon name="search" />
         </template>
@@ -20,13 +14,13 @@
   <div class="q-ma-lg q-pa-md justify-center">
     <div
       class="row items-center justify-center q-gutter-md"
-      style="max-width: 1210px; margin: auto; margin-bottom: 30px;"
+      style="max-width: 1210px; margin: auto; margin-bottom: 30px"
     >
       <div class="text-subtitle1 text-weight-medium">5 Tempat Katering Terenak</div>
       <router-link
         to="/tempat-baru"
         class="text-grey text-caption q-ml-auto"
-        style="text-decoration: none;margin-right: 30px"
+        style="text-decoration: none; margin-right: 30px"
       >
         Lihat lainnya<q-icon name="chevron_right" />
       </router-link>
@@ -40,20 +34,20 @@
   <div class="q-ma-lg q-pa-md justify-center">
     <div
       class="row items-center justify-center q-gutter-md"
-      style="max-width: 1210px; margin: auto; margin-bottom: 30px; "
+      style="max-width: 1210px; margin: auto; margin-bottom: 30px"
     >
       <div class="text-subtitle1 text-weight-medium">5 Tempat Katering Tebaru</div>
       <router-link
         to="/tempat-baru"
         class="text-grey text-caption q-ml-auto"
-        style="text-decoration: none; margin-right: 30px;"
+        style="text-decoration: none; margin-right: 30px"
       >
         Lihat lainnya<q-icon name="chevron_right" />
       </router-link>
     </div>
 
     <div class="row justify-center q-gutter-md q-mb-xl">
-     <KateringCard v-for="(item1, i) in items1" :key="i" :item="item1" :linkable="true" />
+      <KateringCard v-for="(item1, i) in items1" :key="i" :item="item1" :linkable="true" />
     </div>
   </div>
   <!-- Footer -->
@@ -70,77 +64,83 @@ import FooterComponent from '../layouts/FooterComponent.vue'
 
 const items = [
   {
-    id:1,
+    id: 1,
     name: ' Akasya Catering & Co',
-    location: 'Jakarta Selatan',
+    location: 'Jl. Bangka XI No.67-69, Pela Mampang, Jakarta Selatan',
     category: 'Katering',
     rating: '4.4',
     image: '../src/assets/AkasyaCatering.jpeg',
   },
   {
-    name: 'Bungkushin',
-    location: 'Riau',
-    category: 'Jepang',
-    rating: '4.0',
-    image: 'https://linkgambar2.jpg',
+    id: 2,
+    name: 'Umara Catering',
+    location: ' Jl. Intan RSPP No. 23B, Jakarta Selatan',
+    category: 'Katering',
+    rating: '4.7',
+    image: '../src/assets/UmaraCatering.png',
   },
   {
-    name: 'Sancha',
-    location: 'Gading Serpong',
-    category: 'Minuman',
-    rating: '4.0',
-    image: 'https://linkgambar3.jpg',
+    id: 3,
+    name: 'Puspa Catering',
+    location: 'Jl. Pancoran Timur No. 21, Jakarta Selatan',
+    category: 'Katering',
+    rating: '4.5',
+    image: '../src/assets/PuspaCatering.png',
   },
   {
-    name: 'Sancha',
-    location: 'Gading Serpong',
-    category: 'Minuman',
-    rating: '4.0',
-    image: 'https://linkgambar3.jpg',
+    id: 4,
+    name: 'Dapur Bu Sastro',
+    location: 'Jl. Minangkabau Timur No.13, Pasar Manggis, Kecamatan Setiabudi, Jakarta Selatan',
+    category: 'Katering',
+    rating: '4.8',
+    image: '../src/assets/DapurBuSastro.jpeg',
   },
   {
-    name: 'Sancha',
-    location: 'Gading Serpong',
-    category: 'Minuman',
-    rating: '4.0',
-    image: 'https://linkgambar3.jpg',
+    id: 5,
+    name: 'Alfabet Catering',
+    location: 'Jl. Minangkabau Barat No.24, Pasar Manggis, Jakarta Selatan',
+    category: 'Katering',
+    rating: '4.8',
+    image: '../src/assets/AlfabetCatering.png',
   },
 ]
 const items1 = [
   {
-    id: 1,
+    id: 6,
     name: 'Lala Catering',
-    location: 'Jakarta Timur',
+    location: 'Jl. SDN 01 No.124 Pondok Ranggon, Jakarta Timur',
     category: 'Katering',
     rating: '4.9',
     image: '../src/assets/LalaCatering.png',
   },
   {
-    id: 2,
-    name: "Rasa Nyonya Catering",
-    location: 'Jakarta Selatan',
+    id: 7,
+    name: 'Rasa Nyonya Catering',
+    location: 'Jl. Kelapa Dua Raya No.19 F Blok N 3, Kelapa Dua, Tangerang Regency',
     category: 'Katering',
-    rating: '5.0',
+    rating: '3.9',
     image: '../src/assets/RasaNyonya.jpeg',
   },
   {
-    id: 3,
+    id: 8,
     name: ' Adhika Catering',
-    location: 'Jakarta Pusat',
+    location: 'Jl. Zamrud Raya No. 842, Sumur Batu, Jakarta Pusat',
     category: 'Katering',
     rating: '4.5',
     image: '../src/assets/AdhikaCatering.jpeg',
   },
   {
+    id: 9,
     name: 'Plataran Catering Services',
-    location: 'Jakarta Selatan',
+    location: ' Jl. Brawijaya Raya No. 4, Kebayoran Baru, Jakarta Selatan',
     category: 'Katering',
     rating: '5.0',
     image: '../src/assets/PlataranCatering.jpeg',
   },
   {
+    id: 10,
     name: 'Mealfit',
-    location: 'Jakarta Barat',
+    location: 'Jl. Pembangunan III No. 4, Jakarta Barat',
     category: 'Katering',
     rating: '4.8',
     image: '../src/assets/MealFit.jpeg',
